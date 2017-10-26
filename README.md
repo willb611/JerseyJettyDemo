fcc51ca6e77adae00d1c65f592e387ec45e6f30f
William A. O. Brown <will.b611@gmail.com>
2017-10-26 23:29:53 +0100

    Add an README, and re-organise imports

---
+++ README.md
@@ -0,0 +1,11 @@
+## What is this?
+An example HTTP web service template project with an embedded jetty server, with JAX-RS API annotations and jersey as the implementation for JAX-RS.
+### It's running, now what?
+View the example services:
+
+#### Healthcheck
+- [localhost:9999/healthCheck](localhost:9999/healthCheck)
+
+#### View the hello world service
+- [localhost:9999/hello](localhost:9999/hello)
+- [localhost:9999/hello/Will](localhost:9999/hello/Will)
--- src/main/java/com/github/willb611/services/HelloWorldService.java
+++ src/main/java/com/github/willb611/services/HelloWorldService.java
@@ -1,5 +1,8 @@
 package com.github.willb611.services;
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 
@@ -9,7 +12,7 @@ public class HelloWorldService {
   @GET
   @Produces(MediaType.TEXT_PLAIN)
   public String sayHelloWorldFrom() {
-    String result = "Hello, stranger";
+    String result = "Hello, stranger.";
     System.out.println(result);
     return result;
   }
@@ -18,7 +21,7 @@ public class HelloWorldService {
   @Produces(MediaType.TEXT_PLAIN)
   @Path("{from}")
   public String sayHelloWorldFrom(@PathParam("from") String from) {
-    String result = "Hello, " + from;
+    String result = "Hello, " + from + ".";
     System.out.println(result);
     return result;
   }
